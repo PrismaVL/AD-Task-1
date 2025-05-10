c8f4e880e0c1c5f8acbdb295934b2e311d7c2b58
Jay <janjayjil@gmail.com>
2025-05-10 20:53:20 +0800

    feat (index.php) added a class on every input type text

--- index.php
+++ index.php
@@ -10,19 +10,19 @@
     <h2> Sign Up </h2>
     <form method="post">
         <label for="first-name">First Name</label>
-        <input type="text" id= first-name" name="first_name" required>
+        <input type="text" class="text" id= first-name" name="first_name" required>
 
         <label for="middle-name">Middle Name</label>
-        <input type="text" id= middle-name" name="middle_name" required>
+        <input type="text" class="text" id= middle-name" name="middle_name" required>
 
         <label for="last-name">Last Name</label>
-        <input type="text" id= last-name" name="last_name" required>
+        <input type="text" class="text" id= last-name" name="last_name" required>
 
         <label for="birthdate">Birthdate</label>
         <input type="date" id= birthdate" name="birthdate" required>
 
         <label for="address">Address</label>
-        <input type="text" id= address" name="address" required>
+        <input type="text" class="text" id= address" name="address" required>
 
         <label for="email">Email</label>
         <input type="email" id= gmail" name="gmail" required>
