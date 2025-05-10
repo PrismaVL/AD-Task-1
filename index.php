de116ceddd4808714571cd221ec6107f2c634bb0
Jay <janjayjil@gmail.com>
2025-05-10 21:44:05 +0800

    refactor (index.php) removed the onclick and renamed the id of the input password and checkbox , connected the script.js to the index.php

--- index.php
+++ index.php
@@ -10,33 +10,35 @@
     <h2>Get started with your account</h2>
     <form method="post">
         <label for="first-name">First Name</label>
-        <input type="text" class="text" id= first-name" name="first_name" required>
+        <input type="text" class="text" id= "first-name" name="first_name" required>
 
         <label for="middle-name">Middle Name</label>
-        <input type="text" class="text" id= middle-name" name="middle_name" required>
+        <input type="text" class="text" id= "middle-name" name="middle_name" required>
 
         <label for="last-name">Last Name</label>
-        <input type="text" class="text" id= last-name" name="last_name" required>
+        <input type="text" class="text" id= "last-name" name="last_name" required>
 
         <label for="birthdate">Birthdate</label>
-        <input type="date" id= birthdate" name="birthdate" required>
+        <input type="date" id= "birthdate" name="birthdate" required>
 
         <label for="address">Address</label>
-        <input type="text" class="text" id= address" name="address" required>
+        <input type="text" class="text" id= "address" name="address" required>
 
         <label for="email">Email</label>
-        <input type="email" id= gmail" name="gmail" required>
+        <input type="email" id= "gmail" name="gmail" required>
 
         <label for="number">Number</label>
-        <input type="tel" class="text" id= number" name="number" pattern="0-9"{11} placeholder="09260652013" required>
+        <input type="tel" class="text" id= "number" name="number" pattern="0-9"{11} placeholder="09260652013" required>
 
-        <label for="password">Password</label>
-        <input type="password" id= password" name="password" required>
-        <input type="checkbox" onclick="myFunction()">Show Password
+        <label for="mypassword">Password</label>
+        <input type="password" id="mypassword" name="password" required>
 
-
+        <input type="checkbox" id="showPassword">
+        <label for="showPassword">Show Password</label>
+        
         <button type="submit">Register</button>
     </form>
         </div>
+        <script src="assets/js/script.js" defer></script>
     </body>
 </html>
