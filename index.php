b78a96dfa42a7ff419fe8be63b12ee952cef6150
Jay <janjayjil@gmail.com>
2025-05-10 19:33:29 +0800

    feat (index.php) added the base content of the starting page

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Sign Up</title>
+
+    </head>
+    <body>
+        <div class="container">
+    <h2> Sign Up </h2>
+    <form method="post">
+        <label for="first-name">First Name</label>
+        <input type="text" id= first-name" name="first_name" required>
+
+        <label for="middle-name">Middle Name</label>
+        <input type="text" id= middle-name" name="middle_name" required>
+
+        <label for="last-name">Last Name</label>
+        <input type="text" id= last-name" name="last_name" required>
+
+        <label for="birthdate">Birthdate</label>
+        <input type="date" id= birthdate" name="birthdate" required>
+
+        <label for="address">Address</label>
+        <input type="text" id= address" name="address" required>
+
+        <label for="email">Email</label>
+        <input type="email" id= email" name="email" required>
+
+        <label for="number">Number</label>
+        <input type="text" id= number" name="number" required>
+
+        <button type="submit">Register</button>
+    </form>
+        </div>
+    </body>
+</html>
