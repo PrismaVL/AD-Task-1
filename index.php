89f921511c9aae4ababe20669f7e371955c96d14
Jay <janjayjil@gmail.com>
2025-05-10 19:47:16 +0800

    feat (index.php) added a meta attribute and linked the index.php to styles.css

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <meta charset="utf-8">
         <title> Sign Up</title>
-
+        <link href="assets/css/styles.css" media="all" rel="stylesheet">
     </head>
     <body>
         <div class="container">
