1630d69ea8249feb73d854ce3a64448bfa106b79
Jay <janjayjil@gmail.com>
2025-05-11 20:26:46 +0800

    feat (index1.phhp) added a new php file

---
+++ page/index1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title> Sign Up</title>
+        <link href="assets/css/styles.css" media="all" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container">
+
+    </div>
+    </body>
+</html>
