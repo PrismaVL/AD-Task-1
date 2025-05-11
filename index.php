ec7016cb6d9e2c130b8f4b855a279fe7dbc6f5b7
Jay <janjayjil@gmail.com>
2025-05-11 21:17:03 +0800

    refactor (index.php)

--- index.php
+++ index.php
@@ -36,40 +36,39 @@
         <div class="error"><?php echo $error; ?></div>
     <?php endif; ?>
 
-    <form method="post" action="index1.php>
-        <label for="first-name">First Name</label>
-        <input type="text" class="text" id= "first-name" name="first_name" required>
+<form method="post" action="index1.php">
+    <label for="first-name">First Name</label>
+    <input type="text" class="text" id="first-name" name="first_name" required>
 
-        <label for="middle-name">Middle Name</label>
-        <input type="text" class="text" id= "middle-name" name="middle_name" required>
+    <label for="middle-name">Middle Name</label>
+    <input type="text" class="text" id="middle-name" name="middle_name">
 
-        <label for="last-name">Last Name</label>
-        <input type="text" class="text" id= "last-name" name="last_name" required>
+    <label for="last-name">Last Name</label>
+    <input type="text" class="text" id="last-name" name="last_name" required>
 
-        <label for="birthdate">Birthdate</label>
-        <input type="date" id= "birthdate" name="birthdate" required>
+    <label for="birthdate">Birthdate</label>
+    <input type="date" id="birthdate" name="birthdate" required>
 
-        <label for="address">Address</label>
-        <input type="text" class="text" id= "address" name="address" required>
+    <label for="address">Address</label>
+    <input type="text" class="text" id="address" name="address" required>
 
-        <label for="email">Email</label>
-        <input type="email" id= "gmail" name="gmail" required>
+    <label for="email">Email</label>
+    <input type="email" id="email" name="email" required>
 
-        <label for="number">Number</label>
-        <input type="tel" class="text" id= "number" name="number" pattern="{11}" placeholder="09260652013" required>
+    <label for="number">Number</label>
+    <input type="tel" class="text" id="number" name="number" pattern="[0-9]{11}" placeholder="09260652013" required>
 
-        <label for="mypassword">Password</label>
-        <input type="password" id="mypassword" name="password" required>
+    <label for="mypassword">Password</label>
+    <input type="password" id="mypassword" name="password" required>
 
-        <div class="show-password">
-            <input type="checkbox" id="showPassword">
-            <label for="showPassword">Show Password</label>
-        </div>
-
-        <div class="button-container">
-            <button type="submit">Register</button>
-        </div>  
-    </form>
+    <div class="show-password">
+        <input type="checkbox" id="showPassword">
+        <label for="showPassword">Show Password</label>
+    </div>
+    <div class="button-container">
+        <button type="submit">Register</button>
+    </div>  
+</form>
     </div>
         <script src="assets/js/script.js" defer></script>
     </body>
