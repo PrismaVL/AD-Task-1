c5673004ef86839b2b4816fd27d959d252284b64
Jay <janjayjil@gmail.com>
2025-05-13 03:22:22 +0800

    feat(index1.php) Update page title to uppercase in PHP basics demo

--- page/index1.php
+++ page/index1.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Basics</title>
+    <title>PHP BASICS</title>
     <link rel="stylesheet" href="assets/css/styles1.css">
 </head>
 <body>
-    <h1>PHP Basics</h1>
+    <h1>PHP BASICS</h1>
     
     <div class="container">
         <div class="box">
